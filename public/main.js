7613c6a6fb2c54a353d8f0204ee540afb752a621
kalupa <paul@karrupa.com>
2013-01-16 13:32:33 -0500

    heroku setup

--- public/main.js
+++ public/main.js
@@ -11,13 +11,32 @@
       client_id: '97c9ab904f4aeadba33c8efc1c967b08',
       redirect_uri: 'http://localhost:4567/connected'
     });
-    SC.connect(function connect() {
-
-      SC.get('/me',function get(me) {
-        $('app').innerHTML(me.username);
-      });
-    });
+    SC.connect(
+       function() {
+        console.log('connect');
+        SC.get('/me',function get(me) {
+          console.log('get /me');
+          $('app').appendChild((createText(me.username)));
+        });
+        SC.get('/me/activities', function(items){
+          var collection = items.collection;
+          for (var item in collection) {
+            console.log(collection[item].origin.title);
+          }
+        });
+      }
+    );
   };
+
+  function renderTrack(track) {
+    var htmlEl = creatEl('div');
+    htmlEl.id = ''
+
+    var title = createText(track.title);
+    var description = createText(track.description);
+
+    return htmlEl;
+  }
 
   doc.addEventListener('DOMContentLoaded', loadPage);
 
